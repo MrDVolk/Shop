cbb0d12f4cd5642964fd79d4d5ea869ecd859cc8
TheNoice <61051924+TheNoice@users.noreply.github.com>
2020-03-31 16:31:14 +0300

    Initial commit

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IAllCars allCars, ICarsCategory carsCategory)
+        {
+            _allCars = allCars;
+            _allCategories = carsCategory;
+        }
+
+        [Route("Cars/List")]
+        [Route("Cars/List/{category}")]
+        public IActionResult List(string category)
+        {
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.Id);
+            }
+            else
+            {
+                if (string.Equals("electro", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.CategoryName.Equals("Electromobiles")).OrderBy(i => i.Id);
+                    currCategory = "Electromobiles";
+                }
+                else if (string.Equals("gasoline", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.CategoryName.Equals("Combustion engine cars")).OrderBy(i => i.Id);
+                    currCategory = "Combustion engine cars";
+                }
+            }
+
+            
+
+            var carObj = new CarsListViewModel { AllCars = cars, CurrCategory = currCategory };
+
+            ViewBag.Title = "Page with automobiles";
+            return View(carObj);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+        public IActionResult Index()
+        {
+            var homeCars = new HomeViewModel { FavCars = _carRep.GetFavCars };
+
+            return View(homeCars);
+        }
+    }
+}
